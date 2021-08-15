cba8123d2ed486949c6e58790290d6858be1e557
YourContender <dream-boy123@ukr.net>
2021-08-15 22:27:26 +0300

    method object

--- index.js
+++ index.js
@@ -1,90 +1,86 @@
-let answer;
-
 const personalMoviesDB = {
-    count: answer,
+    count: 0,
     movies: {},
     actors: {},
     genders: [], 
-    private: false
-}
+    private: false,
 
-function start() {
-    answer = +prompt('quantity see films?');
+    start: function() {
+        this.count = +prompt('quantity see films?');
 
-    while (answer == '' || answer == null || isNaN(answer)) {
-        answer = +prompt('quantity see films?');
-    }
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt('quantity see films?');
+        }
+    
+        personalMoviesDB.count = this.count;  
+    },
 
-    personalMoviesDB.count = answer;
-}
-start();
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const res = prompt('first film?'),
+                  res1 = prompt('how stars?');
+    
+            if (res != null && res1 != null && res != '' && res1 != '' && res.length <= 50) {
+                personalMoviesDB.movies[res] = res1;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const res = prompt('first film?'),
-              res1 = prompt('how stars?');
-
-        if (res != null && res1 != null && res != '' && res1 != '' && res.length <= 50) {
-            personalMoviesDB.movies[res] = res1;
-            console.log('done');
+    detectPersonalLevel: function() {
+        if (personalMoviesDB.count < 10) {
+            console.log('many films');
+        } else if (personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
+            console.log('classical');
         } else {
-            console.log('error');
-            i--;
+            console.log('professional');
+        }
+    },
+
+    showMyDB: function() {
+        if (!this.private) {
+            console.log('public object: ', personalMoviesDB);
+        } else {
+            console.log('private object');
+        }
+    },
+
+    writeYourGenders: function() {
+        for (let i = 1; i < 2; i++) {
+            let ans = prompt('favorite category films list: ');
+
+            if (ans === '' || ans === null) {
+                console.log('don`t correct data');
+                i--;
+            } else {
+                personalMoviesDB.genders = ans.split(', ');
+            }
+
+        }
+
+        personalMoviesDB.genders.forEach((item, index) => {
+            console.log(`${index + 1}: favorite ${item}`);
+        })
+    },
+
+    toggleVisibleMyDB: function() {
+        if (!this.private) {
+            this.private = true
+        } else {
+            this.private = false
         }
     }
 }
 
-rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMoviesDB.count < 10) {
-        console.log('many films');
-    } else if (personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
-        console.log('classical');
-    } else {
-        console.log('professional');
-    }
-}
-
-detectPersonalLevel();
-
-// if (personalMoviesDB.count < 10) {
-//     console.log('many films');
-// } else if (personalMoviesDB.count > 10 && personalMoviesDB.count < 30) {
-//     console.log('classical');
-// } else {
-//     console.log('professional');
-// }
-
-// console.log(personalMoviesDB);
+personalMoviesDB.start();
+personalMoviesDB.rememberMyFilms();
+personalMoviesDB.detectPersonalLevel();
+personalMoviesDB.showMyDB();
+personalMoviesDB.writeYourGenders();
+personalMoviesDB.toggleVisibleMyDB();
 
 
-function showMyDB(value) {
-    if (!value) {
-        console.log('public object: ', personalMoviesDB);
-    } else {
-        console.log('private object');
-    }
-}
-showMyDB(personalMoviesDB.private);
-
-// function showMyDB(obj) {
-//     for (let item in obj) {
-//         if (item === 'private') {
-//             obj[item] === false ? console.log('this public obj: ', obj) : null
-//         } else {
-//             continue
-//         }  
-//     }
-// }
-// showMyDB(personalMoviesDB)
-
-function writeYourGenders() {
-    for (let i = 1; i <= 3; i++) {
-        let ans = prompt('favorite films');
-
-        personalMoviesDB.genders.push(ans);
-    }
-}
-
-writeYourGenders()
